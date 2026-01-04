0945e5f6028ff3edb03474b7732f6b7a62db61f4
suavmoy samanta <suvamoysamanta907@gmail.com>
2026-01-04 20:38:50 +0530

    Nextjs_Authentication

---
+++ Authentication_nextjs202/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+       This is the  Authentication systems in Next_js how backend in create in Nextjs and how signup and signin will be happen 
+    </div>
+  );
+}
